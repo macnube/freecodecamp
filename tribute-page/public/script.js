6a6e24c1f6b4796534f469333d9217c98964d2cd
Paul McCloud <paul.mccloud@gmail.com>
2016-09-20 16:58:16 -0300

    Added carousel

--- tribute-page/public/script.js
+++ tribute-page/public/script.js
@@ -1,1 +1,24 @@
-console.log("test");
+$(document).ready( function() {
+
+	$(".carousel").slick({
+		"autoplay": true,
+		"dots": true,
+		"pauseOnDotsHover": true,
+		"variableWidth": true,
+		"slidesToShow": 1,
+		"centerMode": true,
+		"autoplaySpeed": 1500,
+	});
+
+	var carouselImages = Array.prototype.slice.call($(".carousel-image"));
+
+	carouselImages.forEach(function(img) {
+		img.onload = function() {
+			var topMargin = (500 - this.height) / 2;
+			$(img).css("margin-top", topMargin);
+		}
+	})
+
+
+
+})
